a0f1599f704a4b7cf5c4cdd8e524bc4ab5ce1936
jeongdopark <jeongdo.dev@gmail.com>
2025-03-04 23:15:31 +0900

    install fumadocs

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -19,7 +19,7 @@ export const baseOptions: BaseLayoutProps = {
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        My App
+        Jeongdo Park
       </>
     ),
   },
@@ -27,6 +27,11 @@ export const baseOptions: BaseLayoutProps = {
     {
       text: 'Documentation',
       url: '/docs',
+      active: 'nested-url',
+    },
+    {
+      text: 'Posting',
+      url: '/blog',
       active: 'nested-url',
     },
   ],
